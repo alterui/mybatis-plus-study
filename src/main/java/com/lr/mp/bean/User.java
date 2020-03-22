9f5c9adf54e1a163284ec9d8fb6a557c97051eb9
alterui <656812771@qq.com>
2020-03-22 21:17:32 +0800

    add bean

--- src/main/java/com/lr/mp/bean/User.java
+++ src/main/java/com/lr/mp/bean/User.java
@@ -4,16 +4,22 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author liurui
  * @date 2020/3/21 10:00
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private Long id;
+
+    private Integer age;
     private String name;
-    private int age;
+
     private String email;
 }
--- src/test/java/com/lr/mp/MybatisPlusStudyApplicationTests.java
+++ src/test/java/com/lr/mp/MybatisPlusStudyApplicationTests.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lr.mp.bean.User;
 import com.lr.mp.mapper.UserMapper;
@@ -12,6 +13,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import javax.swing.plaf.synth.SynthOptionPaneUI;
@@ -19,6 +21,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -117,7 +120,6 @@ class MybatisPlusStudyApplicationTests {
     public void testSelectByPage() {
 
 
-
         Page<User> page = new Page<>(1, 2);//参数一是当前页，参数二是每页个数
         IPage<User> userIPage = userMapper.selectPage(page, null);
         List<User> list = userIPage.getRecords();
@@ -127,8 +129,69 @@ class MybatisPlusStudyApplicationTests {
 
 
     @Test
-    void contextLoads() {
+    public void testSelectByWrapper() {
+        QueryWrapper<User> query = Wrappers.query();
+        query.nested(e -> e.eq("name", "Tom").or().eq("age", 20));
+
+        List<User> users = userMapper.selectList(query);
+        users.forEach(System.out::println);
 
     }
+
+
+    /**
+     * 过滤不需要展示的列
+     */
+    @Test
+    public void testSelectByWrapper1() {
+        QueryWrapper<User> query = Wrappers.query();
+        query.select(User.class, e -> !e.getColumn().equals("age"));
+        List<User> users = userMapper.selectList(query);
+        users.forEach(System.out::println);
+
+    }
+
+
+    @Test
+    public void testSelectByWrapper2() {
+
+        List<User> users = userMapper.selectList(null);
+        users.forEach(System.out::println);
+
+    }
+
+    @Test
+    public void testSelectByWrapper3() {
+        QueryWrapper<User> query = Wrappers.query();
+        query.select("name", "age");
+        List<User> users = userMapper.selectList(query);
+        users.forEach(System.out::println);
+
+    }
+
+    @Test
+    public void testSelectByWrapper4() {
+        condition("Tom", null);
+    }
+
+    private void condition(String name, String age) {
+        QueryWrapper<User> query = Wrappers.query();
+        query.eq(!StringUtils.isEmpty(name), "name", name).eq(!StringUtils.isEmpty(age), "age", age);
+        List<User> users = userMapper.selectList(query);
+        users.forEach(System.out::println);
+
+    }
+
+
+    @Test
+    public void testSelectByWrapper5() {
+        User user = new User();
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>(user);
+
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+
+    }
+
 
 }
